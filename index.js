fd2ab0c631a88d5b983b984ae899322a19884c6c
Raheem <raheemtheallstar137@yahoo.com>
2018-10-28 15:18:40 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,19 @@
+add(a,b){
+  var results = a + b;
+  return results;
+}
+
+subtract(a,b){
+  var results = a - b;
+  return results;
+}
+
+multiply(a,b){
+  var results = a * b;
+  return results;
+}
+
+divide(a,b){
+  var results = a / b;
+  return results;
+}
